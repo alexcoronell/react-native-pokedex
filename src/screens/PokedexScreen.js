01e9a8e21026e8465cfabb47dae172a347857d29
Alex Coronell <alexander@catapultsteel.com>
2024-07-18 17:36:28 -0500

    Basic load pokemons in Pokedex screen

--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,10 +1,27 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { getPokemonApi } from '../api/pokemon'
 
 export default function PokedexScreen() {
+  const [pokemons, setPokemons] = useState([])
+  useEffect(() => {
+    (async () => await loadPokemons())()
+  }, [])
+
+  const loadPokemons = async() => {
+    try {
+      const pokemons = await getPokemonApi()
+      setPokemons(await pokemons)
+      console.log(pokemons)
+    } catch(e) {
+      console.error(e)
+    }
+  }
   return (
     <View>
       <Text>PokedexScreen</Text>
+      <Text>Name: {pokemons[0]?.name}</Text>
     </View>
   )
 }
