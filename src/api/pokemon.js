a5a347ed579a186f93050a8918e47331a213094c
Alex Coronell <alexander@catapultsteel.com>
2024-07-18 17:56:36 -0500

    Basic load pokemons Details in Pokedex screen

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -6,7 +6,17 @@ export async function getPokemonApi() {
     const response = await fetch(url);
     const result = await response.json();
     return result;
-  } catch (e) {
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getPokemonDetailsByUrlApi(url) {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
     throw error;
   }
 }
--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,27 +1,38 @@
-import { View, Text } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
 
-import { getPokemonApi } from '../api/pokemon'
+import { getPokemonApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function PokedexScreen() {
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState([]);
   useEffect(() => {
-    (async () => await loadPokemons())()
-  }, [])
+    (async () => await loadPokemons())();
+  }, []);
 
-  const loadPokemons = async() => {
+  const loadPokemons = async () => {
     try {
-      const pokemons = await getPokemonApi()
-      setPokemons(await pokemons)
-      console.log(pokemons)
-    } catch(e) {
-      console.error(e)
+      const response = await getPokemonApi();
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const res = await getPokemonDetailsByUrlApi(pokemon.url);
+        const pokemonDetail = {
+          id: res.id,
+          name: res.name,
+          type: res.types[0].type.name,
+          order: res.order,
+          image: res.sprites.other["official-artwork"].front_default,
+        };
+        pokemonsArray.push(pokemonDetail);
+      }
+      setPokemons(await pokemonsArray)
+    } catch (e) {
+      console.error(e);
     }
-  }
+  };
   return (
     <View>
       <Text>PokedexScreen</Text>
       <Text>Name: {pokemons[0]?.name}</Text>
     </View>
-  )
+  );
 }
