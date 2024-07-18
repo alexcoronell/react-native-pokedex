e4219613a0f515b165eb3674c64a0a32b9b7413e
Alex Coronell <alexander@catapultsteel.com>
2024-07-18 17:17:35 -0500

    Pokemon screem created

--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
 
 import PokedexScreen from '../screens/PokedexScreen'
+import PokemonScreen from '../screens/PokemonScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -9,6 +10,7 @@ export default function PokedexNavigation() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Pokedex" component={PokedexScreen} options={{ title: "Pokedex "}} />
+      <Stack.Screen name="Pokemon" component={PokemonScreen} options={{ title: "Pokemon "}} />
     </Stack.Navigator>
   )
 }
