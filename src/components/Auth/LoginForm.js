065b05982b43690f8ebd06a065f3d208fdf95174
Alex Coronell <alexander@catapultsteel.com>
2024-07-19 14:05:06 -0500

    LoginForm basic layout

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Keyboard,
+} from "react-native";
+import React from "react";
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm...</Text>
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput style={styles.input} placeholder="Nombre de Usuario" autoCapitalize="none" />
+      <TextInput style={styles.input} placeholder="Contraseña" autoCapitalize="none" secureTextEntry={true} />
+      <Button title="Entrar" onPress={() => console.log('Entrando...')} />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
+  }
+});
