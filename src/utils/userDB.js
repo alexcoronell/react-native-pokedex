3d4c38436679b2e4b57c7c81ed47cc8fc4d8971d
Alex Coronell <alexander@catapultsteel.com>
2024-07-19 13:56:48 -0500

    UserData and Login>Form components created

---
+++ src/utils/userDB.js
@@ -0,0 +1,10 @@
+export const user = {
+    username: "johndoe",
+    password: "123456"
+}
+export const userDetails = {
+    username: "johndoe",
+    firstname: "John",
+    lastname: "Doe",
+    email: "johndoe@email.com"
+}
